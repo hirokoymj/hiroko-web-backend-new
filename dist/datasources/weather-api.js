80b8668c101f285ec7cf6b49f7c9da29d2dfaa86
hirokoymj <hiroko@hirokoymj.com>
2025-07-27 10:35:09 -0500

    updated weather api appkey

--- dist/datasources/weather-api.js
+++ dist/datasources/weather-api.js
@@ -1,33 +1,33 @@
-import { RESTDataSource } from "apollo-datasource-rest";
-import get from "lodash/get.js";
-import map from "lodash/map.js";
-const apiKey = "be2d43efb7b89c5d69256d7ec44da9b8";
+import { RESTDataSource } from 'apollo-datasource-rest';
+import get from 'lodash/get.js';
+import map from 'lodash/map.js';
+const apiKey = '4c7be587c84ac188dca5d7b86e565b11';
 export default class WeatherAPI extends RESTDataSource {
     constructor() {
         super();
-        this.baseURL = "https://api.openweathermap.org/";
+        this.baseURL = 'https://api.openweathermap.org/';
     }
     async getCurrentWeatherByCity(city, unit) {
         const response = await this.get(`data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`);
-        const id = get(response, "id"); // City Id
+        const id = get(response, 'id'); // City Id
         const cityInfo = {
-            name: get(response, "name", ""),
-            country: get(response, "sys.country", ""),
-            lon: get(response, "coord.lon"),
-            lat: get(response, "coord.lat"),
+            name: get(response, 'name', ''),
+            country: get(response, 'sys.country', ''),
+            lon: get(response, 'coord.lon'),
+            lat: get(response, 'coord.lat'),
         };
         const weather = {
-            dt: get(response, "dt"),
-            condition: get(response, "weather[0].main"),
-            description: get(response, "weather[0].description"),
-            icon: get(response, "weather[0].icon"),
+            dt: get(response, 'dt'),
+            condition: get(response, 'weather[0].main'),
+            description: get(response, 'weather[0].description'),
+            icon: get(response, 'weather[0].icon'),
             temperature: {
-                day: get(response, "main.temp"),
-                min: get(response, "main.temp_min"),
-                max: get(response, "main.temp_max"),
+                day: get(response, 'main.temp'),
+                min: get(response, 'main.temp_min'),
+                max: get(response, 'main.temp_max'),
             },
-            feelsLike: get(response, "main.feels_like"),
-            humidity: get(response, "main.humidity"),
+            feelsLike: get(response, 'main.feels_like'),
+            humidity: get(response, 'main.humidity'),
         };
         return {
             id,
@@ -38,23 +38,23 @@ export default class WeatherAPI extends RESTDataSource {
     async getDailyForecast(city, unit) {
         const days = 7;
         const response = await this.get(`data/2.5/forecast/daily?q=${city}&units=${unit}&cnt=${days}&appid=${apiKey}`);
-        const id = get(response, "city.id");
+        const id = get(response, 'city.id');
         const cityInfo = {
-            name: get(response, "city.name"),
-            country: get(response, "city.country"),
-            lon: get(response, "city.coord.lon", 0),
-            lat: get(response, "city.coord.lat", 0),
+            name: get(response, 'city.name'),
+            country: get(response, 'city.country'),
+            lon: get(response, 'city.coord.lon', 0),
+            lat: get(response, 'city.coord.lat', 0),
         };
-        const list = get(response, "list", []);
+        const list = get(response, 'list', []);
         const mappedData = map(list, (data) => {
             const { dt, temp, humidity, speed, sunrise, sunset } = data;
-            const { main, icon, description } = get(data, "weather[0]", []);
+            const { main, icon, description } = get(data, 'weather[0]', []);
             const temperature = {
-                day: get(temp, "day", 0),
-                min: get(temp, "min", 0),
-                max: get(temp, "max", 0),
+                day: get(temp, 'day', 0),
+                min: get(temp, 'min', 0),
+                max: get(temp, 'max', 0),
             };
-            const rain = get(data, "rain") == null ? 0 : get(data, "rain");
+            const rain = get(data, 'rain') == null ? 0 : get(data, 'rain');
             return {
                 dt,
                 condition: description,
@@ -74,7 +74,7 @@ export default class WeatherAPI extends RESTDataSource {
         };
     }
 }
-// TEST URL - Current Weather
-// https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=be2d43efb7b89c5d69256d7ec44da9b8
-// TEST URL - DailyForecast
-// https://api.openweathermap.org/data/2.5/forecast/daily?q=tokyo&units=metric&cnt=7&appid=be2d43efb7b89c5d69256d7ec44da9b8
+//TEST URL
+// https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=4c7be587c84ac188dca5d7b86e565b11
+// https://api.openweathermap.org/data/2.5/forecast/daily?q=tokyo&units=metric&cnt=7&appid=4c7be587c84ac188dca5d7b86e565b11
+// https://api.openweathermap.org/data/2.5/forecast/daily?q=tokyo&units=metric&cnt=7&appid=4c7be587c84ac188dca5d7b86e565b11
